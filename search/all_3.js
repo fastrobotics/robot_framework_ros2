bc7beaec2285a8ab7347da0d5c06a3c50794b990
dgitz <davidgitz@gmail.com>
2026-09-10 01:10:55 +0000

    Deploying to gh-pages from  @ e9c1817565c51a93f6ea4f55ce18ed92484a9f7d 🚀

--- search/all_3.js
+++ search/all_3.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['b_3a_20clone_20this_20repo_20into_20your_20repo_0',['1.2 Option B: Clone this repo into your repo',['../md_dev__tools_2README.html#autotoc_md3',1,'']]]
+  ['b_3a_20clone_20this_20repo_20into_20your_20repo_0',['1.2 Option B: Clone this repo into your repo',['../md_dev__tools_2README.html#autotoc_md3',1,'']]],
+  ['build_1',['Build',['../md_README.html#autotoc_md12',1,'']]]
 ];
